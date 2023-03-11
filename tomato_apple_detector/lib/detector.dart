b03488daf6e51adbb82310a39d9185377ac0e051
James Michael E. Paz <105730089+JmePaz@users.noreply.github.com>
2023-03-11 14:57:05 +0800

    Update detector.dart
    Build a image picker from android gallery

--- tomato_apple_detector/lib/detector.dart
+++ tomato_apple_detector/lib/detector.dart
@@ -1,6 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Detector extends StatefulWidget {
   const Detector({super.key});
@@ -10,13 +11,30 @@ class Detector extends StatefulWidget {
 }
 
 class _DetectorState extends State<Detector> {
+  late File _img;
+  bool isSetImg = false;
+
+  Future<void> selectImg() async {
+    final img = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (img!.path != null) {
+      setState(() {
+        isSetImg = true;
+        _img = File(img.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Tomato/Apple Detector"),
       ),
-      body: Column(children: []),
+      body: Column(children: [
+        (isSetImg) ? Image.file(_img) : Container(),
+        ElevatedButton(
+            onPressed: selectImg, child: const Text("Select an Image"))
+      ]),
     );
   }
 }
