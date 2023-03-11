906ecc92fb207e74cc47ea92d74bedba35b0e348
James Michael E. Paz <105730089+JmePaz@users.noreply.github.com>
2023-03-11 19:35:04 +0800

    Update detector.dart
    add a detected output features on model

--- tomato_apple_detector/lib/detector.dart
+++ tomato_apple_detector/lib/detector.dart
@@ -1,7 +1,9 @@
+import 'dart:ffi';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 class Detector extends StatefulWidget {
   const Detector({super.key});
@@ -13,6 +15,21 @@ class Detector extends StatefulWidget {
 class _DetectorState extends State<Detector> {
   late File _img;
   bool isSetImg = false;
+  var detectedOutput = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    Tflite.close();
+  }
 
   Future<void> selectImg() async {
     final img = await ImagePicker().pickImage(source: ImageSource.gallery);
@@ -21,7 +38,26 @@ class _DetectorState extends State<Detector> {
         isSetImg = true;
         _img = File(img.path);
       });
+      predictImage(_img);
     }
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
+  }
+
+  void predictImage(File img) async {
+    var output = await Tflite.runModelOnImage(
+        path: img.path,
+        numResults: 2,
+        threshold: 0.5,
+        imageMean: 127.5,
+        imageStd: 127.5);
+    setState(() {
+      detectedOutput = output!;
+    });
+    // print("Result are: ${detectedOutput}");
   }
 
   @override
@@ -31,9 +67,33 @@ class _DetectorState extends State<Detector> {
         title: const Text("Tomato/Apple Detector"),
       ),
       body: Column(children: [
-        (isSetImg) ? Image.file(_img) : Container(),
-        ElevatedButton(
-            onPressed: selectImg, child: const Text("Select an Image"))
+        (isSetImg)
+            ? Container(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Image.file(_img))
+            : Container(),
+        Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(color: Colors.redAccent),
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Center(
+                child: Text(
+              (detectedOutput.isNotEmpty)
+                  ? "${detectedOutput[0]["label"].split(" ")[1]}: ${detectedOutput[0]["confidence"].toStringAsFixed(2)}"
+                  : "Select an Image First",
+              style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white),
+            ))),
+        Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: ElevatedButton(
+                onPressed: selectImg,
+                child: const Text(
+                  "Select an Image",
+                  style: TextStyle(fontSize: 20),
+                )))
       ]),
     );
   }
